ced589df39f028a2ef48ff276953d07d66a3d15f
Jimmy Voskuil <jimmyvoskuil@msn.com>
2016-10-05 16:31:41 +0200

    added google api for contact page

---
+++ includes/main.js
@@ -0,0 +1,13 @@
+// Google Maps on the contact page
+
+function initMap() {
+        var uluru = {lat: 52.341099 lng: 4.823463};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
