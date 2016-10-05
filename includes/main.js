12d86373eb4d6c3edf6a69c05b5d4280a14e2ffc
Selma Dorrestein <selmadorrestein@gmail.com>
2016-10-05 17:51:41 +0200

    added paragraphs to philosophy.html, added lots of jQ to main.js to try how to make a slideshowthing happen

--- includes/main.js
+++ includes/main.js
@@ -1,3 +1,49 @@
-//test to see whether it conflicts
-$("div #imagePlantation").hide();
-$("div #imagePlantation").fadeIn(3000);
+//fadeOut image/fadeIn tekst
+$(document).ready(function(){
+	$(".textPlantation").hide();
+
+	// $("body").scroll(function(){
+	// 	console.log('scroll triggered')
+	// 	$("#imagePlantation").fadeOut(3000);
+	// });
+	$(document).on( 'scroll', function(){
+		console.log('Event Fired');
+	});
+
+	$("#imagePlantation").click(function(){
+		$(".textPlantation").fadeIn(3000);
+	});
+})
+
+//gebruik evt css manipulation
+//de formule hierboven geeft nu aan hoevaak er gescrolld is.
+
+//Dit is dat hij outfade als je een lange pagina hebt. dus helaas niet wat we zoeken.
+// $(window).scroll(function () {
+//     var scrollTop = $(window).scrollTop();
+//     var height = $(window).height();
+//     $("#imagePlantation").css({
+//         'opacity': ((height - scrollTop) / height)
+//     });
+// });
+
+
+// $("#imagePlantation").animate({ height: 'toggle', opacity: 'toggle' }, 5000);
+// $("#imagePlantation").animate({ height: 0, opacity: 0 }, 5000);
+
+// function slideFade(elem) {
+//    var fade = { opacity: 1, transition: 'opacity 0.5s' };
+//    elem.css(fade).slideUp(5000);
+//    }
+// slideFade($('#imagePlantation'));
+
+
+
+
+
+
+// $(window).scroll(function() {
+//     $("#imagePlantation").css({
+//     'opacity' : 1-(($(this).scrollTop())/100)
+//     });          
+// });
