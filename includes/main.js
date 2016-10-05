cf1b55d055e8f4b85ab65c16323b22d725ab1fe7
Jimmy Voskuil <jimmyvoskuil@msn.com>
2016-10-05 16:36:51 +0200

    solved merge conflicts

--- includes/main.js
+++ includes/main.js
@@ -1,3 +1,16 @@
 //test to see whether it conflicts
 $("div #imagePlantation").hide();
 $("div #imagePlantation").fadeIn(3000);
+// Google Maps on the contact page
+
+function initMap() {
+        var uluru = {lat: 52.341099 lng: 4.823463};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
